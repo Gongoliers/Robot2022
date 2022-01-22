fa8272f39a889cac36399907799e88a38b4a23bd
Richard Buckley <42151145+rhbuckley@users.noreply.github.com>
2022-01-22 13:18:53 -0500

    Created Development Branch For Drivetrain
    Slowly creating the necessary files for the drivetrain and updating new functions. I would not expect this to compile.

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,33 @@
+package frc.robot;
+
+public class RobotMap {
+    
+    // Drivetrain
+    // --> PWM (motors; see pulse width modulation)
+    public static final int LEFT_DRIVE_PWM = 0;
+    public static final int RIGHT_DRIVE_PWM = 1;
+
+    // Encoders
+    public static final int LEFT_DRIVE_ENCODER_A = 0;
+    public static final int LEFT_DRIVE_ENCODER_B = 0;
+    public static final int RIGHT_DRIVE_ENCODER_A = 0;
+    public static final int RIGHT_DRIVE_ENCODER_B = 0;
+
+    // Endgame
+    // public static final int
+    // public static final int
+    // public static final int
+    // public static final int
+    
+    // Intake
+    // public static final int
+    // public static final int
+    // public static final int
+    // public static final int
+
+    // Shooter
+    // public static final int
+    // public static final int
+    // public static final int
+    // public static final int
+}
