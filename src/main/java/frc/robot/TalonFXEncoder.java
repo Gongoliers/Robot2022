102fca6f7b6235a09763d2468da843f4102e9d36
Kyle Corry <kylecorry31@gmail.com>
2022-03-06 07:18:23 -0500

    Extract talon FX encoders and leverage Gongolierium encoder sensors

--- src/main/java/frc/robot/TalonFXEncoder.java
+++ src/main/java/frc/robot/TalonFXEncoder.java
@@ -1,8 +1,24 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.thegongoliers.input.odometry.BaseEncoderSensor;
+
 /**
  * Allows for the conversion of the TalonFX Encoder Value to an Encoder 
  */
-public class TalonFXEncoder {
+public class TalonFXEncoder extends BaseEncoderSensor {
+
+    private WPI_TalonFX mTalonFX;
+
+    public TalonFXEncoder(WPI_TalonFX talonFX) {
+        super(talonFX::getSelectedSensorPosition, talonFX::getSelectedSensorVelocity);
+        mTalonFX = talonFX;
+        useIntegratedSensor();
+    }
+
+    public void useIntegratedSensor(){
+        mTalonFX.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+    }
     
 }
