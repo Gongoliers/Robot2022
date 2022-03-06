6dbd9a4c8357f05379fd2461922bf09cafc90b6e
Kyle Corry <kylecorry31@gmail.com>
2022-03-06 07:34:16 -0500

    Create phoenix encoder to support different controllers and sensors

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.kauailabs.navx.frc.AHRS;
 import com.thegongoliers.output.drivetrain.ModularDrivetrain;
@@ -17,7 +18,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.NavxGyro;
-import frc.robot.IntegratedTalonFXEncoder;
+import frc.robot.PhoenixMotorControllerEncoder;
 import frc.robot.Constants.DriveConstants;
 
 public class DrivetrainSubsystem extends SubsystemBase {
@@ -65,15 +66,15 @@ public class DrivetrainSubsystem extends SubsystemBase {
         
         // Encoders
         var leftAverageEncoder = new AverageEncoderSensor(
-            new IntegratedTalonFXEncoder(m_leftMotor_1),
-            new IntegratedTalonFXEncoder(m_leftMotor_2),
-            new IntegratedTalonFXEncoder(m_leftMotor_3)
+            new PhoenixMotorControllerEncoder(m_leftMotor_1, FeedbackDevice.IntegratedSensor),
+            new PhoenixMotorControllerEncoder(m_leftMotor_2, FeedbackDevice.IntegratedSensor),
+            new PhoenixMotorControllerEncoder(m_leftMotor_3, FeedbackDevice.IntegratedSensor)
         );
 
         var rightAverageEncoder = new AverageEncoderSensor(
-            new IntegratedTalonFXEncoder(m_rightMotor_1),
-            new IntegratedTalonFXEncoder(m_rightMotor_2),
-            new IntegratedTalonFXEncoder(m_rightMotor_3)
+            new PhoenixMotorControllerEncoder(m_rightMotor_1, FeedbackDevice.IntegratedSensor),
+            new PhoenixMotorControllerEncoder(m_rightMotor_2, FeedbackDevice.IntegratedSensor),
+            new PhoenixMotorControllerEncoder(m_rightMotor_3, FeedbackDevice.IntegratedSensor)
         );
 
         m_leftEncoderSensor = leftAverageEncoder.scaledBy(Constants.DriveConstants.kEncoderDistancePerPulse);
