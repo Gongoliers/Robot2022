ee07852276d54a784203b6f29cc79ef343e6f7e5
Richard Buckley <42151145+rhbuckley@users.noreply.github.com>
2022-03-12 13:15:53 -0500

    Added TODO

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -105,6 +105,7 @@ public class RobotContainer {
     m_raise_endgame.whileHeld(raiseEndgameCommand);
     m_raise_endgame.whenReleased(engageSafetyCommand);
 
+    // TODO: CONFIGURE THIS => SEPARATE disengageSafety / engageSafety
     JoystickButton m_lower_endgame = new JoystickButton(m_driverJoystick, 8);
     var lowerEndgamecommand = new LowerMotor(m_endgameSubsystem);
     m_lower_endgame.whenPressed(disengageSafetyCommand);
