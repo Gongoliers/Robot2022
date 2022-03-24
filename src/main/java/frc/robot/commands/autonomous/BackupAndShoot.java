59941c970fc94444b338895de3003fc4ed7328d5
Kyle Corry <kylecorry31@gmail.com>
2022-03-24 13:50:31 -0400

    Use simple drive distance command

--- src/main/java/frc/robot/commands/autonomous/BackupAndShoot.java
+++ src/main/java/frc/robot/commands/autonomous/BackupAndShoot.java
@@ -3,6 +3,7 @@ package frc.robot.commands.autonomous;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.commands.drivetrain.DriveDistance;
+import frc.robot.commands.drivetrain.SimpleDriveDistance;
 import frc.robot.commands.shooter.ShootHigh;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
@@ -10,8 +11,9 @@ import frc.robot.subsystems.ShooterSubsystem;
 public class BackupAndShoot extends SequentialCommandGroup {
     public BackupAndShoot(DrivetrainSubsystem drivetrain, ShooterSubsystem shooter){
         addCommands(
-            new DriveDistance(drivetrain, (double) AutoConstants.kDistanceToDriveForHigh).withTimeout(1.5), // AFTER TESTING IT SEEMS OPTIMAL LOCATION FOR SHOOTING FOR HIGH IS 20.5 INCHES
-            new ShootHigh(shooter).withTimeout(8.0)
+                new SimpleDriveDistance(drivetrain, AutoConstants.kDistanceToDriveForHigh, 0.1, 0.25).withTimeout(1.5),
+//            new DriveDistance(drivetrain, (double) AutoConstants.kDistanceToDriveForHigh).withTimeout(1.5), // AFTER TESTING IT SEEMS OPTIMAL LOCATION FOR SHOOTING FOR HIGH IS 20.5 INCHES
+                new ShootHigh(shooter).withTimeout(8.0)
         );
     }
 }
