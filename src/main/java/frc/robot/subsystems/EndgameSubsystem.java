9480755232ea1ce7ab66f401b44752b48aadfdbe
Hayden Heroux <haydenheroux@gmail.com>
2022-03-29 14:26:35 -0400

    Use EndgameArm in EndgameSubsystem.

--- src/main/java/frc/robot/subsystems/EndgameSubsystem.java
+++ src/main/java/frc/robot/subsystems/EndgameSubsystem.java
@@ -11,54 +11,39 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.EndgameTimer;
 import frc.robot.InvertableLimitSwitch;
+import frc.robot.EndgameArm;
 import frc.robot.PhoenixMotorControllerEncoder;
 import frc.robot.Constants.EndgameConstants;
 
 public class EndgameSubsystem extends SubsystemBase {
 
-    enum EndgameArmState {
-        STOPPED,
-        ASCENDING,
-        DESCENDING
-    }
-
     // Left Motor (when standing behind battery)
     private final WPI_TalonSRX m_motorA = new WPI_TalonSRX(EndgameConstants.kMotorACAN);
     private EncoderSensor m_encoderA = new PhoenixMotorControllerEncoder(m_motorA, FeedbackDevice.CTRE_MagEncoder_Relative);
     private InvertableLimitSwitch m_limitSwitchA = new InvertableLimitSwitch(EndgameConstants.kLimitSwitchAPort);
-    private EndgameArmState m_motorAState = EndgameArmState.STOPPED;
+    private final EndgameArm m_endgameA = new EndgameArm(m_motorA, m_encoderA, m_limitSwitchA);
 
     // Right Motor (when standing behind battery)
     private final WPI_TalonSRX m_motorB = new WPI_TalonSRX(EndgameConstants.kMotorBCAN);
     private EncoderSensor m_encoderB = new PhoenixMotorControllerEncoder(m_motorB, FeedbackDevice.CTRE_MagEncoder_Relative);
     private InvertableLimitSwitch m_limitSwitchB = new InvertableLimitSwitch(EndgameConstants.kLimitSwitchBPort);
-    private EndgameArmState m_motorBState = EndgameArmState.STOPPED;
+    private final EndgameArm m_endgameB = new EndgameArm(m_motorB, m_encoderB, m_limitSwitchB);
 
     // Initializing Pneumatics
     private final Solenoid m_unlockArms = new Solenoid(PneumaticsModuleType.CTREPCM, EndgameConstants.kSolenoidCAN);
 
-
     public EndgameSubsystem() {
-        /** Configuring Encoder Values
-         *
-         * See __ for documentation regarding Encoders:
-         * https://docs.ctre-phoenix.com/en/stable/ch14_MCSensor.html#cross-the-road-electronics-magnetic-encoder-absolute-and-relative
-         *
-         * See __ for documentation regarding FeedbackDevice Constants:
-         * https://store.ctr-electronics.com/content/api/cpp/html/namespacectre_1_1phoenix_1_1motorcontrol.html#a76df6b51b79bdd3710ddcd6ef43050e7
-         */
 
-        m_encoderA.reset();
-        m_encoderB.reset();
+		m_endgameA.getMotor().setInverted(true);
+		m_endgameA.getLimitSwitch().setInverted(true);
+		m_endgameA.getEncoder().reset();
 
-        m_limitSwitchA.setInverted(true);
-        m_limitSwitchB.setInverted(true);
+		m_endgameB.getMotor().setInverted(true);
+		m_endgameB.getLimitSwitch().setInverted(true);
+		m_endgameB.getEncoder().reset();
 
         // Ensure that Solenoid is Unpowered
         m_unlockArms.set(false);
-
-        m_motorA.setInverted(true);
-        m_motorB.setInverted(true);
 
     }
 
@@ -68,46 +53,17 @@ public class EndgameSubsystem extends SubsystemBase {
         }
     }
 
-    public AverageEncoderSensor getEncoders() {
-        return new AverageEncoderSensor(m_encoderA, m_encoderB);
-    }
+	public void enableAscension() {
+		m_endgameA.disengageSafety();
+		m_endgameB.disengageSafety();
+        m_unlockArms.set(true);
+	}
 
-    public void updateEndgameA() {
-        switch (m_motorAState) {
-            case ASCENDING:
-                if (m_encoderA.getDistance() >= EndgameConstants.kCappedDistanceA) {
-                    m_motorAState = EndgameArmState.STOPPED;
-                }
-                break;
-            case DESCENDING:
-                if (m_limitSwitchA.isTriggered()) {
-                    m_motorAState = EndgameArmState.STOPPED;
-                    m_encoderA.reset();
-                }
-                break;
-            case STOPPED:
-                break;
-        }
-    }
-
-    public void updateEndgameB() {
-        switch (m_motorBState) {
-            case ASCENDING:
-                if (m_encoderB.getDistance() >= EndgameConstants.kCappedDistanceB) {
-                    m_motorBState = EndgameArmState.STOPPED;
-                }
-                break;
-            case DESCENDING:
-                if (m_limitSwitchB.isTriggered()) {
-                    m_motorBState = EndgameArmState.STOPPED;
-                    m_encoderB.reset();
-                }
-                break;
-            case STOPPED:
-                break;
-        }
-    }
-
+	public void disableAscension() {
+		m_endgameA.disengageSafety();
+		m_endgameB.disengageSafety();
+        m_unlockArms.set(false);
+	}
 
     @Override
     public void periodic() {
