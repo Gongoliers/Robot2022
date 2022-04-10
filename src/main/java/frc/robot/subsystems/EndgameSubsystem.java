1164a71231fabc8151b216c37d3399e4a778eb8f
Kyle Corry <kylecorry31@gmail.com>
2022-04-10 06:39:22 -0400

    Formatting

--- src/main/java/frc/robot/subsystems/EndgameSubsystem.java
+++ src/main/java/frc/robot/subsystems/EndgameSubsystem.java
@@ -18,56 +18,55 @@ import frc.robot.Constants.EndgameConstants;
 
 public class EndgameSubsystem extends SubsystemBase {
 
-	// Initializing Pneumatics
-	private final Solenoid m_armUnlockControllerA = new Solenoid(PneumaticsModuleType.CTREPCM, EndgameConstants.kSolenoidACAN);
-	private final Solenoid m_armUnlockControllerB = new Solenoid(PneumaticsModuleType.CTREPCM, EndgameConstants.kSolenoidBCAN);
+    // Initializing Pneumatics
+    private final Solenoid m_armUnlockControllerA = new Solenoid(PneumaticsModuleType.CTREPCM, EndgameConstants.kSolenoidACAN);
+    private final Solenoid m_armUnlockControllerB = new Solenoid(PneumaticsModuleType.CTREPCM, EndgameConstants.kSolenoidBCAN);
 
     // Left Motor (when standing behind battery)
     private final WPI_TalonFX m_motorA = new WPI_TalonFX(EndgameConstants.kMotorACAN);
     private EncoderSensor m_encoderA = new PhoenixMotorControllerEncoder(m_motorA, FeedbackDevice.IntegratedSensor)
-		.scaledBy(EndgameConstants.kCappedDistanceA);
+            .scaledBy(EndgameConstants.kCappedDistanceA);
     private InvertableLimitSwitch m_limitSwitchA = new InvertableLimitSwitch(EndgameConstants.kLimitSwitchAPort);
-	/**
-	 * Drive Arms Up x desired feet (important record distance on both smartdashboard and mark arm w sharpie TICKS * FEET/TICK)(use other program)
-	 * Set caluclated value to top distance (half to not overdrive)
-	 * 
-	 * Oscilating = on one test
-	 * 
-	 * 1. Change EndgameConstants.kTopFeet to tuned ^^
-	 * 2. Change P Value to reach mark marked on endgame arm consistantly
-	 * 		2a. It's better for the endgame arm to hit the mark and continue around it than to not reach mark
-	 * 3. Change D value (start at 0.02) until the EndgameArm stops moving around that point. This does not matter
-	 * 			if it stops directly on that point (just need arm to stop moving around)
-	 * 4. If D does not make arm stop at line, use I term (middle) (start at 0.01) (always positive tune until arm
-	 *		 reaches point)
-	 * 
-	 */
-	private GSpeedController m_controllerA = new GSpeedController(m_motorA, m_encoderA, new PID(0.5, 0.0, 0.0), new PID(0.0, 0.0, 0.0));
+    /**
+     * Drive Arms Up x desired feet (important record distance on both smartdashboard and mark arm w sharpie TICKS * FEET/TICK)(use other program)
+     * Set caluclated value to top distance (half to not overdrive)
+     * <p>
+     * Oscilating = on one test
+     * <p>
+     * 1. Change EndgameConstants.kTopFeet to tuned ^^
+     * 2. Change P Value to reach mark marked on endgame arm consistantly
+     * 2a. It's better for the endgame arm to hit the mark and continue around it than to not reach mark
+     * 3. Change D value (start at 0.02) until the EndgameArm stops moving around that point. This does not matter
+     * if it stops directly on that point (just need arm to stop moving around)
+     * 4. If D does not make arm stop at line, use I term (middle) (start at 0.01) (always positive tune until arm
+     * reaches point)
+     */
+    private GSpeedController m_controllerA = new GSpeedController(m_motorA, m_encoderA, new PID(0.5, 0.0, 0.0), new PID(0.0, 0.0, 0.0));
     private final EndgameArm m_endgameA = new EndgameArm(m_controllerA, m_encoderA, m_limitSwitchA, m_armUnlockControllerA);
 
     // Right Motor (when standing behind battery)
     private final WPI_TalonFX m_motorB = new WPI_TalonFX(EndgameConstants.kMotorBCAN);
     private EncoderSensor m_encoderB = new PhoenixMotorControllerEncoder(m_motorB, FeedbackDevice.IntegratedSensor)
-		.scaledBy(EndgameConstants.kCappedDistanceB);
+            .scaledBy(EndgameConstants.kCappedDistanceB);
     private InvertableLimitSwitch m_limitSwitchB = new InvertableLimitSwitch(EndgameConstants.kLimitSwitchBPort);
-	private GSpeedController m_controllerB = new GSpeedController(m_motorB, m_encoderB, new PID(0.5, 0.0, 0.0), new PID(0.0, 0.0, 0.0));
+    private GSpeedController m_controllerB = new GSpeedController(m_motorB, m_encoderB, new PID(0.5, 0.0, 0.0), new PID(0.0, 0.0, 0.0));
     private final EndgameArm m_endgameB = new EndgameArm(m_controllerB, m_encoderB, m_limitSwitchB, m_armUnlockControllerB);
 
     public EndgameSubsystem() {
-		m_encoderA.reset();
-		m_encoderB.reset();
+        m_encoderA.reset();
+        m_encoderB.reset();
 
-		m_motorA.setInverted(true);
-		m_motorB.setInverted(true);
-		
-		m_limitSwitchA.setInverted(true);
-		m_limitSwitchB.setInverted(true); // TODO: Check if limit switches are replaced
+        m_motorA.setInverted(true);
+        m_motorB.setInverted(true);
+
+        m_limitSwitchA.setInverted(true);
+        m_limitSwitchB.setInverted(true); // TODO: Check if limit switches are replaced
     }
 
-	public void run(EndgameAction action){
-    	m_endgameA.run(action);
-    	m_endgameB.run(action);
-	}
+    public void run(EndgameAction action) {
+        m_endgameA.run(action);
+        m_endgameB.run(action);
+    }
 
     @Override
     public void periodic() {
