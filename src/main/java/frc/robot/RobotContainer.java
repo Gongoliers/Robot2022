6d8005aaeb24295bfbbeb8a41be983cfde088986
Gongoliers P&C <gongoliers5112@gmail.com>
2022-04-11 16:42:53 -0400

    Add new autos for single cargo shooting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,7 +19,8 @@ import frc.robot.Constants.OIConstants;
 import frc.robot.DPadButton.Direction;
 import frc.robot.commands.StopAll;
 import frc.robot.commands.autonomous.BackupAndShoot;
-import frc.robot.commands.autonomous.BackupAndShootWithDelayAndThenLeaveTarmac;
+import frc.robot.commands.autonomous.BackupAndShootLowThenLeaveTarmac;
+import frc.robot.commands.autonomous.BackupAndShootHighThenLeaveTarmac;
 import frc.robot.commands.autonomous.FullSystemCheck;
 import frc.robot.commands.autonomous.LeaveTarmac;
 import frc.robot.commands.autonomous.LeaveTarmacAndShoot;
@@ -123,7 +124,8 @@ public class RobotContainer {
         autoChooser.addOption("Do Nothing", new StopAll(m_drivetrain, m_endgame, m_shooter, m_intake));
         autoChooser.addOption("System Check", new FullSystemCheck(m_drivetrain));
         autoChooser.addOption("Leave Tarmac", new LeaveTarmac(m_drivetrain));
-        autoChooser.addOption("Shoot then Leave Tarmac", new BackupAndShootWithDelayAndThenLeaveTarmac(m_drivetrain, m_shooter));
+        autoChooser.addOption("Shoot Low then Leave Tarmac", new BackupAndShootLowThenLeaveTarmac(m_drivetrain, m_shooter));
+        autoChooser.addOption("Shoot High then Leave Tarmac", new BackupAndShootHighThenLeaveTarmac(m_drivetrain, m_shooter));
         autoChooser.addOption("Leave Tarmac Shoot", new LeaveTarmacAndShoot(m_drivetrain, m_shooter, m_intake));
         autoChooser.setDefaultOption("Leave Tarmac Shoot", new LeaveTarmacAndShoot(m_drivetrain, m_shooter, m_intake));
     
---
+++ src/main/java/frc/robot/commands/autonomous/BackupAndShootLowThenLeaveTarmac.java
@@ -0,0 +1,23 @@
+package frc.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.commands.drivetrain.Drive;
+import frc.robot.commands.drivetrain.SimpleDriveDistance;
+import frc.robot.commands.shooter.ShootLow;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class BackupAndShootLowThenLeaveTarmac extends SequentialCommandGroup {
+    public BackupAndShootLowThenLeaveTarmac(DrivetrainSubsystem drivetrain, ShooterSubsystem shooter){
+        addCommands(
+                new WaitCommand(2.0),
+                new SimpleDriveDistance(drivetrain, AutoConstants.kDistanceToDriveForHigh, 0.05, 0.3).withTimeout(3.0),
+                new ShootLow(shooter).withTimeout(8.0),
+                new WaitCommand(1.5),
+                new Drive(drivetrain, -Constants.AutoConstants.kAutoDriveToFenderSpeed).withTimeout(Constants.AutoConstants.kAutoDriveToFenderSeconds)
+        );
+    }
+}
