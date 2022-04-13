29fa994c40a0a883acb88b089fbd7647f14d4639
Kyle Corry <kylecorry31@gmail.com>
2022-04-13 12:11:49 -0400

    Add configurable wait time to leave tarmac

--- src/main/java/frc/robot/commands/autonomous/LeaveTarmac.java
+++ src/main/java/frc/robot/commands/autonomous/LeaveTarmac.java
@@ -1,14 +1,26 @@
 package frc.robot.commands.autonomous;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.commands.drivetrain.Drive;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class LeaveTarmac extends SequentialCommandGroup {
-    public LeaveTarmac(DrivetrainSubsystem drivetrain){
+
+    public LeaveTarmac(DrivetrainSubsystem drivetrain, double waitTime) {
         addCommands(
-            new Drive(drivetrain, Constants.AutoConstants.kAutoDriveAwayFromFenderSpeed).withTimeout(Constants.AutoConstants.kAutoDriveAwayFromFenderSeconds)
+                new WaitCommand(waitTime),
+                getDriveCommand(drivetrain)
         );
+    }
+
+    public LeaveTarmac(DrivetrainSubsystem drivetrain) {
+        addCommands(getDriveCommand(drivetrain));
+    }
+
+    private Command getDriveCommand(DrivetrainSubsystem drivetrain) {
+        return new Drive(drivetrain, Constants.AutoConstants.kAutoDriveAwayFromFenderSpeed).withTimeout(Constants.AutoConstants.kAutoDriveAwayFromFenderSeconds);
     }
 }
